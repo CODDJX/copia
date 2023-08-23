faeeeeea63bf110541b9f0f083b7e4eed69e132e
CODDJX <137509812+CODDJX@users.noreply.github.com>
2023-08-23 11:24:34 -0300

    Update HostMercy.py

--- HostMercy.py
+++ HostMercy.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import socket
 import threading
 
@@ -7,10 +6,27 @@ import threading
 DEFAULT_NUM_THREADS = 200
 
 def scan_ips(start_ip, end_ip):
-    # Your existing scan_ips function here...
+    ips = []
+    start_ip_split = list(map(int, start_ip.split('.')))
+    end_ip_split = list(map(int, end_ip.split('.')))
+
+    for i in range(start_ip_split[0], end_ip_split[0] + 1):
+        for j in range(start_ip_split[1], end_ip_split[1] + 1):
+            for k in range(start_ip_split[2], end_ip_split[2] + 1):
+                for l in range(start_ip_split[3], end_ip_split[3] + 1):
+                    ip = f"{i}.{j}.{k}.{l}"
+                    ips.append(ip)
+
+    return ips
 
 def test_ip(ip, port):
-    # Your existing test_ip function here...
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.settimeout(1)
+            sock.connect((ip, port))
+        return True
+    except (socket.error, socket.timeout):
+        return False
 
 def find_cameras(start_ip, end_ip, port, num_threads=DEFAULT_NUM_THREADS):
     ips = scan_ips(start_ip, end_ip)
@@ -20,7 +36,7 @@ def find_cameras(start_ip, end_ip, port, num_threads=DEFAULT_NUM_THREADS):
 
     class CameraScanner(threading.Thread):
         def __init__(self, ip_list):
-            threading.Thread.__init__(self)
+            super().__init__()
             self.ip_list = ip_list
 
         def run(self):
@@ -28,7 +44,7 @@ def find_cameras(start_ip, end_ip, port, num_threads=DEFAULT_NUM_THREADS):
             for ip in self.ip_list:
                 if test_ip(ip, port):
                     with open("host.txt", "a+") as f:
-                        existing_ips = set(f.read().splitlines())
+                        existing_ips = set(line.strip() for line in f)
                         if ip not in existing_ips:
                             f.write(f"{ip}\n")
                             saved_ips += 1
@@ -39,7 +55,7 @@ def find_cameras(start_ip, end_ip, port, num_threads=DEFAULT_NUM_THREADS):
     threads = []
     chunk_size = len(ips) // num_threads
     for i in range(num_threads):
-        thread_ips = ips[i*chunk_size:(i+1)*chunk_size]
+        thread_ips = ips[i * chunk_size:(i + 1) * chunk_size]
         thread = CameraScanner(thread_ips)
         threads.append(thread)
         thread.start()
@@ -52,6 +68,7 @@ def find_cameras(start_ip, end_ip, port, num_threads=DEFAULT_NUM_THREADS):
 if __name__ == '__main__':
     start_ip = input("Enter the start IP address range: ")
     end_ip = input("Enter the end IP address range: ")
-    port = input("Enter the port to scan: ")
-    find_cameras(start_ip, end_ip, int(port))
+    port = int(input("Enter the port to scan: "))
+    find_cameras(start_ip, end_ip, port)
+
 
