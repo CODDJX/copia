e482f50be55e77c1400a810ac057039c6a4d7ef1
CODDJX <137509812+CODDJX@users.noreply.github.com>
2023-08-23 11:19:58 -0300

    Update HostMercy.py

--- HostMercy.py
+++ HostMercy.py
@@ -1,34 +1,18 @@
 import os
-import re
 import sys
 import socket
 import threading
-import time
-from datetime import datetime
+
+# Constants
+DEFAULT_NUM_THREADS = 200
 
 def scan_ips(start_ip, end_ip):
-    ips = []
-    start_ip_split = start_ip.split('.')
-    end_ip_split = end_ip.split('.')
-    for i in range(int(start_ip_split[0]), int(end_ip_split[0])+1):
-        for j in range(int(start_ip_split[1]), int(end_ip_split[1])+1):
-            for k in range(int(start_ip_split[2]), int(end_ip_split[2])+1):
-                for l in range(int(start_ip_split[3]), int(end_ip_split[3])+1):
-                    ip = f"{i}.{j}.{k}.{l}"
-                    ips.append(ip)
-    return ips
+    # Your existing scan_ips function here...
 
 def test_ip(ip, port):
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(2)  # Aumenta el tiempo de espera a 5 segundos
-        sock.connect((ip, port))
-        sock.close()
-        return True
-    except:
-        return False
+    # Your existing test_ip function here...
 
-def find_cameras(start_ip, end_ip, port):
+def find_cameras(start_ip, end_ip, port, num_threads=DEFAULT_NUM_THREADS):
     ips = scan_ips(start_ip, end_ip)
     total_ips = len(ips)
     scanned_ips = 0
@@ -51,10 +35,8 @@ def find_cameras(start_ip, end_ip, port):
                 scanned_ips += 1
                 progress = (scanned_ips / total_ips) * 100
                 print(f"\rProgress: {progress:.2f}% | Saved IPs: {saved_ips}", end='', flush=True)
-                time.sleep(0.2)  # Introduce una pausa de 0.5 segundos
 
     threads = []
-    num_threads = 500
     chunk_size = len(ips) // num_threads
     for i in range(num_threads):
         thread_ips = ips[i*chunk_size:(i+1)*chunk_size]
@@ -72,3 +54,4 @@ if __name__ == '__main__':
     end_ip = input("Enter the end IP address range: ")
     port = input("Enter the port to scan: ")
     find_cameras(start_ip, end_ip, int(port))
+
